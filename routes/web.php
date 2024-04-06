55617d5174008a03eb19600f385e333ee7f6df58
RohanMishra <mishrarohan090@gmail.com>
2024-04-06 22:31:25 +0545

    database and few changes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
+// Sign in/ sign up page
 Route::get('/', [UserController::class, 'index'])->name('e_store-login');
 
+//Main Home page
+Route::get('/main', [UserController::class, 'home'])->name('e_store-home');
+
+Route::post('/signUp', [UserController::class, 'signUp'])->name('e_store-signUp');
+
+Route::get('/success', [UserController::class, 'success'])->name('e_store-success');
 
 
-Route::post('/toggle-merchant', [UserController::class, 'toggleMerchant'])->name('toggle.merchant');
