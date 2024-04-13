3c946988a1b5c22695f2d3fb5da37b8cd7adb342
RohanMishra <mishrarohan090@gmail.com>
2024-04-13 11:15:35 +0545

    create store backend finished

--- database/migrations/2024_04_08_151518_create_categories_table.php
+++ database/migrations/2024_04_08_151518_create_categories_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('c_name');
-            $table->foreignId('s_id')->constrained('sub_categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
