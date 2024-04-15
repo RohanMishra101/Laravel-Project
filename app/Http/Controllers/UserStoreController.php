1c483e1a45589bbbae8852b5b2fd63fe021aad88
RohanMishra <mishrarohan090@gmail.com>
2024-04-15 11:14:41 +0545

    test commit

--- app/Http/Controllers/UserStoreController.php
+++ app/Http/Controllers/UserStoreController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Store;
 use Illuminate\Http\Request;
 
 class UserStoreController extends Controller
 {
     public function storePage($storeName)
     {
-        dd($storeName);
+        $storeData = Store::where('store_name', $storeName)->get();
+        // dd($storeData->toArray());
+
+        // dd($storeData->toArray());
+        return view('store.userStore', ['storeData' => $storeData]);
+        // dd($storeName);
         // return view('pages.userStore');
+
     }
 }
