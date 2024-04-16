8bb9275a3864d496e6d3ae70533748c2deba4769
RohanMishra <mishrarohan090@gmail.com>
2024-04-16 19:09:58 +0545

    home page design

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -13,25 +13,8 @@
 
 <body>
     <main>
-        {{-- THis is a testing code donot touch --}}
-        {{-- <h1>This is the main page for browsing products</h1>
-
-        @if (session()->has('user'))
-            <p>{{ session()->get('user')->username }}</p>
-        @else
-            <a href="{{ route('e_store-login') }}">CREATE ACCOUNT</a>
-        @endif --}}
-
-        {{-- <a href="{{ route('e_store-logout') }}">Log Out</a> --}}
-        {{-- THis is a testing code donot touch --}}
-
-
-
-        {{-- main code for this page start here --}}
-        {{-- <h1>This site is under construction</h1> --}}
+        {{-- Nav section --}}
         <section title="nav-section" class="bg-light">
-
-
             <nav class="container navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid px-5">
                     <!-- Logo on the left -->
@@ -90,8 +73,8 @@
                                         @endif
                                     </li>
                                     <li><a class="dropdown-item" href="#">View Profile</a></li>
-                                    <li><a class="dropdown-item" href="{{route('e_store-dashboard')}}">Store</a></li>
-                                    <li><a class="dropdown-item" href="#">Log Out</a></li>
+                                    <li><a class="dropdown-item" href="{{ route('e_store-dashboard') }}">Store</a></li>
+                                    <li><a class="dropdown-item" href="{{ route('e_store-logout') }}">Log Out</a></li>
                                 </ul>
                             </li>
                         </ul>
@@ -101,21 +84,116 @@
 
 
         </section>
-        <section title="carousel-section">
 
+        {{-- Carousel Section --}}
+        <section title="carousel-section">
+            <div id="carouselExampleDark" class="container carousel carousel-dark slide custom-border mt-5">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
+                        aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
+                        aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
+                        aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active custom-size" data-bs-interval="10000">
+                        <img src="{{ asset('images/image1.png') }}" class="d-block w-100 custom-img"
+                            alt="Advertisement 1">
+                        <div class="carousel-caption d-none d-md-block custom-bg">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item custom-size" data-bs-interval="2000">
+                        <img src="{{ asset('images/image2.png') }}" class="d-block w-100 custom-img"
+                            alt="Advertisement 2">
+                        <div class="carousel-caption d-none d-md-block custom-bg">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item custom-size">
+                        <img src="{{ asset('images/image3.png') }}" class="d-block w-100 custom-img"
+                            alt="Advertisement 3">
+                        <div class="carousel-caption d-none d-md-block custom-bg">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </section>
 
+        {{-- Body Section --}}
         <section title="body-section">
-            {{-- First div for categories --}}
-            <div></div>
-
-            {{-- Second div for store Listing --}}
-            <div></div>
+            <div class="container mt-5 ">
+                <div class="row">
+                    <!-- Categories Section -->
+                    <div class="col-md-3 col-12">
+                        <div class="categories-section custom-border">
+                            <!-- Categories List -->
+                            <h1 class="text-center fs-5">Category</h1>
+                            <hr class="custom-hr mb-3">
+                            <ul>
+                                @foreach ($categories as $item)
+                                    <li class="list-group-item mt-3 ">
+                                        <a class="custom-style" href="#">
+                                            {{ $item->c_name }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Items Section -->
+                    <div class="col-md-9 col-12 ">
+                        <div class="container items-section custom-border p-5">
+                            <!-- Search Bar -->
+                            <div class="input-group custom-search mb-3">
+                                <input type="text" class="form-control" placeholder="Search">
+                                <div class="input-group-append ">
+                                    <button class="btn btn-outline-secondary " type="button">Q</button>
+                                </div>
+                            </div>
+                            <!-- Items List -->
+                            <div class="row">
+                                <div class="col-md-3 col-6 mb-3" *ngFor="let item of items">
+                                    <div class="card">
+                                        <img src="path-to-image" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Item 1</h5>
+                                            <p class="card-text">Description</p>
+                                            <p class="card-text">Price $</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Repeat for more items -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     </main>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    <script script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     </script>
 </body>
 
