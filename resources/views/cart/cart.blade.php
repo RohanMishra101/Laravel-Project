006163b928b540b2f052aeb731a898a6f3c5a73d
RohanMishra <mishrarohan090@gmail.com>
2024-04-23 13:49:52 +0545

    few improvements

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-    <title>Document</title>
+    <title>e-store</title>
 </head>
+
 <body>
     {{-- @foreach ($cartDetails as $details)
     @php
@@ -20,68 +22,93 @@
     @endphp --}}
     {{-- <p>{{dd($orderNo)}}</p> --}}
     {{-- <p>{{dd($cartDetails)}}</p> --}}
-    <div class="container custom-product-list border border-dark rounded-4">
-        @foreach ($categories as $category)
-            @php
-                $categoryProducts = $cartItems->where('c_id', $category->id);
-            @endphp
-            @if($categoryProducts->isNotEmpty())
-            <div class="container mt-5" style="margin-bottom: 20px">
-                <div class="row border border-dark rounded-3">
-                    <div class=" p-3">
-                        <h2>{{ $category->c_name }}</h2>
-                        <hr class="w-100">
-                    </div>
-                    {{-- <p>{{dd($cartItems)}}</p> --}}
-                    @foreach ($categoryProducts as $product)
-                        <div class="col-md-3">
-                            <div class="card mb-4">
-                                <img src="{{ asset($product->p_img) }}"
-                                class=" border border-light rounded card-img-top custom-card-img"
-                                alt="{{ $product->p_name }}">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ $product->p_name }}</h5>
-                                    <p class="card-text">{{ $product->p_description }}</p>
-                                    <p class="card-text"><small class="text-muted">Price:
-                                            ${{ $product->p_price }}</small></p>
-                                    @foreach ($cartDetails as $details)
-                                    @if($details->product_id== $product->id)
-                                        <p class="card-text"><small class="text-muted">No. Of Orders:{{ $details->no_of_orders}}</small></p>
-                                        @if($details['order_confirm']==0)
-                                        <form action="/cartItemConfirm/{{ $details['id'] }}" method="post" style="display: inline">
-                                            @csrf
-                                            <button type="submit" id="showConfirm-{{ $details['order_confirm'] }}"class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#EditItemModal">Confirm</button>
-                                        </form>
-                                        @endif
-                                        <button type="submit" id="showDeleteForm-{{ $details['id'] }}" class="btn btn-primary" data-bs-toggle="modal"
-                                        data-bs-target="#EditItemModal">Delete</button>
-                                        <div id="hiddenDeleteForm-{{ $details['id'] }}" style="display: none">
-                                            <form action="/cartItemDelete/{{ $details['id'] }}" method="post">
-                                                @csrf()
-                                                <p>Are you sure you want to remove this from cart?</p>
-                                                <button type="submit">Yes</button>
-                                                <button type="button" onclick="showForm('hiddenDeleteForm-{{ $details['id'] }}')">No</button>
-                                            </form>
-                                        </div>
-                                        {{-- <p>{{ $details['id'] }}</p> --}}
-                                    @endif
-                                    @endforeach
+
+    <main>
+        <section>
+            <div class="container text-center">
+                <h1>
+                    Cart
+                </h1>
+                <hr class="custom-hr">
+            </div>
+            <div class="container custom-product-list ">
+                @foreach ($categories as $category)
+                    @php
+                        $categoryProducts = $cartItems->where('c_id', $category->id);
+                    @endphp
+                    @if ($categoryProducts->isNotEmpty())
+                        <div class="container mt-5" style="margin-bottom: 20px">
+                            <div class="row border border-dark rounded-3">
+                                <div class=" p-3">
+                                    <h2>{{ $category->c_name }}</h2>
+                                    <hr class="w-100">
                                 </div>
-                            </div>
-                        </div>
-                        {{-- @php
+                                {{-- <p>{{dd($cartItems)}}</p> --}}
+                                @foreach ($categoryProducts as $product)
+                                    <div class="col-md-3">
+                                        <div class="card mb-4">
+                                            <img src="{{ asset($product->p_img) }}"
+                                                class=" border border-light rounded card-img-top custom-card-img"
+                                                alt="{{ $product->p_name }}">
+                                            <div class="card-body">
+                                                <h5 class="card-title">{{ $product->p_name }}</h5>
+                                                <p class="card-text overflow-hidden" style="max-height: 40px">
+                                                    {{ $product->p_description }}</p>
+                                                <p class="card-text"><small class="text-muted">Price:
+                                                        ${{ $product->p_price }}</small></p>
+                                                @foreach ($cartDetails as $details)
+                                                    @if ($details->product_id == $product->id)
+                                                        <p class="card-text"><small class="text-muted">No. Of
+                                                                Orders:{{ $details->no_of_orders }}</small></p>
+                                                        @if ($details['order_confirm'] == 0)
+                                                            <form action="/cartItemConfirm/{{ $details['id'] }}"
+                                                                method="post" style="display: inline">
+                                                                @csrf
+                                                                <button type="submit"
+                                                                    id="showConfirm-{{ $details['order_confirm'] }}"class="btn btn-primary"
+                                                                    data-bs-toggle="modal"
+                                                                    data-bs-target="#EditItemModal">Confirm</button>
+                                                            </form>
+                                                        @endif
+                                                        <button type="submit" id="showDeleteForm-{{ $details['id'] }}"
+                                                            class="btn btn-primary" data-bs-toggle="modal"
+                                                            data-bs-target="#EditItemModal">Delete</button>
+                                                        <div id="hiddenDeleteForm-{{ $details['id'] }}"
+                                                            style="display: none">
+                                                            <form action="/cartItemDelete/{{ $details['id'] }}"
+                                                                method="post">
+                                                                @csrf()
+                                                                <p>Are you sure you want to remove this from cart?</p>
+                                                                <button type="submit">Yes</button>
+                                                                <button type="button"
+                                                                    onclick="showForm('hiddenDeleteForm-{{ $details['id'] }}')">No</button>
+                                                            </form>
+                                                        </div>
+                                                        {{-- <p>{{ $details['id'] }}</p> --}}
+                                                    @endif
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {{-- @php
                             $i++;
                         @endphp --}}
-                    @endforeach
-                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    @else
+                        {{-- <p>No products found in this category.</p> --}}
+                    @endif
+                @endforeach
             </div>
-            @else
-            {{-- <p>No products found in this category.</p> --}}
-            @endif
-        @endforeach
-    </div>
-    <script>
+        </section>
+    </main>
 
+
+
+
+
+    <script>
         document.querySelectorAll("[id^='showDeleteForm-']").forEach(function(button) {
             button.addEventListener("click", function() {
                 var productId = this.id.split("-")[1];
@@ -93,7 +120,7 @@
             var div = document.getElementById(a);
             div.style.display = (div.style.display === "none") ? "block" : "none";
         }
-
     </script>
 </body>
+
 </html>
--- resources/views/pages/testOrders.blade.php
+++ resources/views/pages/testOrders.blade.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container custom-product-list border border-dark rounded-4">
-           @forEach($products as $product)
+        @foreach ($products as $product)
             <div class="col-md-3">
                 <div class="card mb-4">
                     <img src="{{ asset($product->p_img) }}"
-                    class=" border border-light rounded card-img-top custom-card-img"
-                    alt="{{ $product->p_name }}">
+                        class=" border border-light rounded card-img-top custom-card-img" alt="{{ $product->p_name }}">
                     <div class="card-body">
                         <h5 class="card-title">{{ $product->p_name }}</h5>
                         <p class="card-text">{{ $product->p_description }}</p>
@@ -24,7 +25,10 @@
                     </div>
                 </div>
             </div>
-           @endforeach
+        @endforeach
     </div>
+
+    
 </body>
+
 </html>
